f043d60c47a6c665d6bcc7fd6b10172a581bf42f
Ym652013 <030238220130@st.buh.edu.vn>
2024-10-16 19:00:06 +0700

    main

--- pages/db_connection.php
+++ pages/db_connection.php
@@ -11,6 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-else
-echo"kết nối thành công!";
 ?>
